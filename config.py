7bb7a5731a9a079de572f430c17e4c417070adb5
d3sm0 <simone.totaro@gmail.com>
2022-01-27 17:50:55 -0500

    Working impala

--- config.py
+++ config.py
@@ -5,15 +5,20 @@ import torch
 DEBUG = sys.gettrace() is not None
 env_id = "CartPole-v0"
 should_render = False
-proc_num = 5
-host = "mila"
-sweep_yaml = "sweep.yaml"
+if DEBUG:
+    proc_num = 1
+    host = ""
+    sweep_yaml = ""
+else:
+    proc_num = 5
+    host = "mila"
+    sweep_yaml = "sweep.yaml"
 
 max_steps = int(1e6)
 actor_lr = 0.0001
-critic_lr = 0.0005
+critic_lr = 0.005
 trajectory_len = 20
-num_actors = 1 if DEBUG else 5
+num_actors = 5
 batch_size = 32
 gamma = 0.99
 save_every = 100
